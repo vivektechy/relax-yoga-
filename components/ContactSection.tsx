53ecb9661eb8d63ed18ff457f4a5ad48761cb828
vivek kumar <vivekchaubey2211@gmail.com>
2026-07-09 18:55:18 +0530

    Update Google Maps iframe source in ContactSection

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -262,7 +262,7 @@ export default function ContactSection() {
             {/* Map */}
             <div className="rounded-2xl overflow-hidden h-48 border border-mint/20 shadow-md">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57975.69682879296!2d85.07372574999999!3d25.594094849999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed58dce6732867%3A0x4059f39a1ac82f06!2sPatna%2C%20Bihar!5e0!3m2!1sen!2sin!4v1699000000000!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.221860528607!2d85.14598837497466!3d25.59754297745548!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed597e0dd4740f%3A0x9db1d61dbaf091f9!2sRelax%20Yoga%20Centre!5e0!3m2!1sen!2sin!4v1783603301459!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
